b6f4f36c4b12c6fdb0cc0279a37445ff687866e5
Klim Lyapin <westn23@mail.ru>
2017-05-04 20:58:43 +0000

    Fixed prints.

--- threading.py
+++ threading.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import threading, socket
+import threading
+import socket
 
 # Number one (binding one thread to TCP-socket).
 class FirstThread(threading.Thread): #Class for my first thread.
@@ -10,29 +11,28 @@ class FirstThread(threading.Thread): #Class for my first thread.
 	self.ip = ip 
 	self.port = port
 	self.socket = socket
-	print '[+] New Thread Started For: ' + ip + ':' + str(port) #Print where the thread is starting.
+	print('[+] New Thread Started For: ', ip, ':', str(port)) #Print where the thread is starting.
 
     def run(self): #My function 'run'
-        print 'Connection from: ' + ip + ':' + str(port) #Print from where somebody is connected.
+        print('Connection from: ', ip, ':', str(port)) #Print from where somebody is connected.
 	self.socket.send('\nWelcome to the server!\n\n') # Sending a message to the client.
 	data = 'mydata'
 	
 	while len(data):
 	    data = self.socket.recv(2048) #Buffer Size '2 KiB'
-	    print 'Client (%s:%s) sent : %s' %(self.ip, str(self.port), data) # Print how much did client with this address send me.
+	    print('Client (%s:%s) sent : %s' %(self.ip, str(self.port), data)) # Print how much did client with this address send me.
 	    self.socket.send('You sent me: ' + data) # Print for client how much did he send me.
-	print 'Client at ' + self.ip +  ' disconneted...' # While not len(data) printing that client at that ip disconnected.
+	print('Client at ', self.ip,  ' disconneted...') # While not len(data) printing that client at that ip disconnected.
 
 host = '0.0.0.0' # host with ip '0.0.0.0', so we can listen all interfaces.
 port = 9999  # and port '9999' TELNET control
-
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Creating an INET and STREAMing socket in variable 'tcp_socket'
 tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Many applications can listen socket.
 tcp_socket.bind((host, port)) # Bind socket to this adress.
 
 while True:
     tcp_socket.listen(4) # Become a server socket.
-    print '\nListening for incoming connections...' # Print that we are waiting for incoming connection.
+    print('\nListening for incoming connections...') # Print that we are waiting for incoming connection.
     (socket, (ip, port)) = tcp_socket.accept() #Accept connections from outside.
     
     newthread = FirstThread(ip, port, socket) # Pass socket to the FirstThread thread object being created.
@@ -40,32 +40,32 @@ while True:
 
 # Number two (Thread display console menu with any text).
 def print_menu():
-    print 30 * '-', 'MENU', 30 * '-' # Lines with MENU at the edge.
-    print '1. Menu Option 1' # 1st menu.
-    print '2. Menu Option 2' # 2nd menu.
-    print '3. Menu Option 3' # 3rd menu.
-    print '4. Menu Option 4' # 4th menu.
-    print '5. Exit' # Exit menu.
-    print 67 * '-' # Lines
+    print(30 * '-', 'MENU', 30 * '-') # Lines with MENU at the edge.
+    print('1. Menu Option 1') 
+    print('2. Menu Option 2') 
+    print('3. Menu Option 3') 
+    print('4. Menu Option 4') 
+    print('5. Exit')
+    print(67 * '-')
 
-loop = True # Variable Loop for while.
+loop = True 
 
-while loop: # While loop = True
-    print_menu() # Display our quite primitive menu.
-    choice = int(raw_input('Enter your choice from 1 to 5: ')) # Choice the number of menu paragraph.
+while loop: 
+    print_menu() 
+    choice = int(raw_input('Enter your choice from 1 to 5: '))
 
-    if choice==1: # If you choice equal 1
-        print 'Menu 1 has been selected' # Print selected paragraph.
-    elif choice==2: # If you choice equal 2
-        print 'Menu 2 has been selected' # Print selected paragraph.
-    elif choice==3: # If you choice equal 3
-        print 'Menu 3 has been selected' # Print selected paragraph.
-    elif choice==4: # If you choice equal 4
-        print 'Menu 4 has been selected' # Print selected paragraph.
-    elif choice==5: # If you choice equal 5
-        print 'You selected \'Exit\'. Good Bye!' # Print selected paragraph.
-        loop=False # This will make the while loop to end as not value of loop is set to False.
+    if choice==1: 
+        print('Menu 1 has been selected')
+    elif choice==2: 
+        print('Menu 2 has been selected')
+    elif choice==3: 
+        print('Menu 3 has been selected')
+    elif choice==4: 
+        print('Menu 4 has been selected') 
+    elif choice==5: 
+        print('You selected \'Exit\'. Good Bye!')
+        loop=False 
     else: 
-        raw_input('Wrong option selection. Enter any key to try again..') # Your choice was not from 1 to 5.
+        raw_input('Wrong option selection. Enter any key to try again..') 
 
 threading.Thread(target = print_menu).start() # Start our menu in threading.
