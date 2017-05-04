d8ae1375d6103a7b0a56df6bda6b184d7f6ae4a7
Klim Lyapin <westn23@mail.ru>
2017-05-04 23:32:28 +0000

    Replace new method TCP in network from server.py Delete all stuff that communicate with network Did it like print

--- network.py
+++ network.py
@@ -18,3 +18,17 @@ class FirstThread(threading.Thread):
             self.socket.send('You sent me: ' + data)
         print('Client at ', self.ip,  ' disconneted...')
 
+    def TCP(self):
+        if __name__ == '__main__':
+            ADDR = (host, port)
+    	    IDDR = (ip, port)
+    	    tcp_socket = socket(AF_INET, SOCK_STREAM)
+   	    tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+  	    tcp_socket.bind(ADDR)
+  	    tcp_socket.listen(4)
+  	    tcp_list[]
+
+            while True:
+                print('\nListening for incoming connections...')
+                tcp_list.append([socket, IDDR]) = tcp_socket.accept()
+
--- server.py
+++ server.py
@@ -8,20 +8,5 @@ from menu import print_menu
 host = '0.0.0.0' 
 port = 9999  
 
-
-if __name__ == '__main__':
-    ADDR = (host, port)
-    IDDR = (ip, port)
-    tcp_socket = socket(AF_INET, SOCK_STREAM) 
-    tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) 
-    tcp_socket.bind(ADDR)
-    tcp_socket.listen(4) 
-    tcp_list[]
-
-    while True:
-        print('\nListening for incoming connections...') 
-        tcp_list.append([socket, IDDR]) = tcp_socket.accept() 
-    
-    FirstThread(IDDR, socket).start() 
-   
-    threading.Thread(target = print_menu).start() 
+FirstThread(target = TCP).start() 
+threading.Thread(target = print_menu).start() 
