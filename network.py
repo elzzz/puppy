2eec1eecaf3237c35cd34a0a4e51c5508e2e79ae
Klim Lyapin <westn23@mail.ru>
2017-05-05 02:32:41 +0000

    I love pylint so much

--- network.py
+++ network.py
@@ -1,17 +1,18 @@
-from socket import *
+from socket import socket
 
-def start_tcp_service(host,port):
 
-   tcp_socket = socket(AF_INET, SOCK_STREAM)
-   tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-   tcp_socket.bind((host, port))
-   tcp_socket.listen(4)
+def start_tcp_service(host, port):
 
-   tcp_dict = {}
+    tcp_socket = socket(AF_INET, SOCK_STREAM)
+    tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    tcp_socket.bind((host, port))
+    tcp_socket.listen(4)
 
-   while True:
-       print('\nListening for incoming connections...')
-       conn, addr = tcp_socket.accept()
-       tcp_dict[addr[0]]= conn
-       print(tcp_dict)
+    tcp_dict = {}
+
+    while True:
+        print('\nListening for incoming connections...')
+        conn, addr = tcp_socket.accept()
+        tcp_dict[addr[0]]= conn
+        print(tcp_dict)
 
