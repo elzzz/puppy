a4197bb16fc4ace17db861b5018e1a4efea8cbdd
Klim Lyapin <westn23@mail.ru>
2017-05-05 00:46:49 +0000

    Delete Vars

--- network.py
+++ network.py
@@ -1,13 +1,11 @@
 def start_tcp_service():
-   ADDR = (host, port)
-   IDDR = (ip, port)
    tcp_socket = socket(AF_INET, SOCK_STREAM)
    tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-   tcp_socket.bind(ADDR)
+   tcp_socket.bind((host, port))
    tcp_socket.listen(4)
    tcp_list[]
 
    while True:
        print('\nListening for incoming connections...')
-       tcp_list.append([socket, IDDR]) = tcp_socket.accept()
+       tcp_list.append([socket, (ip, port)]) = tcp_socket.accept()
 
