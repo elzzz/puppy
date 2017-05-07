84db52f18c87f72974b4a74304318f25ea04cf72
Klim Lyapin <westn23@mail.ru>
2017-05-07 23:46:31 +0000

    Logging network

--- network.py
+++ network.py
@@ -19,7 +19,7 @@ def start_tcp_service(host, port):
     tcp_dict = {}
 
     while True:
-        print('Listening for incoming connections...')
+        print('\nListening for incoming connections...')
 	log.debug('Listening for incoming connections.')
 	conn, addr = tcp_socket.accept()
 	log.debug('Accepting the connection.')
