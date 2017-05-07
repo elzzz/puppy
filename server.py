209b2f7ee1815dff13ac9622610ab8208e3f4b74
Klim Lyapin <westn23@mail.ru>
2017-05-07 23:24:16 +0000

    Logging in file

--- server.py
+++ server.py
@@ -4,9 +4,12 @@ import sys
 import threading
 from network import start_tcp_service
 from menu import print_menu
+from logbook import Logger, StreamHandler
 
 HOST = '0.0.0.0'
 PORT = 9999
+StreamHandler(sys.stdout).push_application()
+log = Logger('Test Logger')
 
 if __name__ == '__main__':
 
@@ -15,13 +18,18 @@ if __name__ == '__main__':
     MENU_THREAD = threading.Thread(target=print_menu)
 
     TCP_SERVICE_THREAD.daemon = True
+    log.debug('Daemoning thread with TCP SERVICE.')
     MENU_THREAD.daemon = True
+    log.debug('Daemoning thread with menu.')
 
     TCP_SERVICE_THREAD.start()
+    log.debug('Starting thread with TCP SERVICE.')
     MENU_THREAD.start()
+    log.debug('Starting thread with menu.')
 
     try:
         while True:
             pass
     except (KeyboardInterrupt, SystemExit):
+        log.debug('Exit')
         sys.exit(0)
