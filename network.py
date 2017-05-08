2a5bee6776bcb30c3760045cd00e05d298e67bfe
Klim Lyapin <westn23@mail.ru>
2017-05-08 18:10:03 +0000

    made some logs with vars and changed it to info

--- network.py
+++ network.py
@@ -3,25 +3,24 @@ from log import get_logger
 
 
 LOG = get_logger()
+NUMBER_OF_CONNECTIONS = 4
 
 def start_tcp_service(host, port):
 
     tcp_socket = socket(AF_INET, SOCK_STREAM)
     tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    LOG.debug('Set options SOL_SOCKET and SO_REUSEADDR.')
+    LOG.info('Set options SOL_SOCKET and SO_REUSEADDR.')
     tcp_socket.bind((host, port))
-    LOG.debug('Bind socket to the address.')
-    tcp_socket.listen(4)
-    LOG.debug('Make number of the maximum connections equal 4.')
+    LOG.info('Bind socket to the address: {}:{}'.format(str(host), str(port)))
+    tcp_socket.listen(NUMBER_OF_CONNECTIONS)
+    LOG.info('''Make number of the maximum connections equal variable
+                                        \'NUMBER_OF_CONNECTIONS\'.''')
 
     tcp_dict = {}
 
     while True:
         print('\nListening for incoming connections...')
-        LOG.debug('Listening for incoming connections.')
+        LOG.info('Listening for incoming connections.')
         conn, addr = tcp_socket.accept()
-        LOG.debug('Accepting the connection.')
         tcp_dict[addr[0]] = conn
-        print(tcp_dict)
-        LOG.debug('Successful connection.')
-
+        LOG.info('Accepting the connection from: {}'.format(str(tcp_dict)))
--- server.py
+++ server.py
@@ -18,18 +18,18 @@ if __name__ == '__main__':
     MENU_THREAD = threading.Thread(target=print_menu)
 
     TCP_SERVICE_THREAD.daemon = True
-    LOG.debug('Daemoning thread with TCP SERVICE.')
+    LOG.info('Daemonizing thread with TCP SERVICE.')
     MENU_THREAD.daemon = True
-    LOG.debug('Daemoning thread with menu.')
+    LOG.info('Daemonizing thread with menu.')
 
     TCP_SERVICE_THREAD.start()
-    LOG.debug('Starting thread with TCP SERVICE.')
+    LOG.info('Starting thread with TCP SERVICE.')
     MENU_THREAD.start()
-    LOG.debug('Starting thread with menu.')
+    LOG.info('Starting thread with menu.')
 
     try:
         while True:
             pass
     except (KeyboardInterrupt, SystemExit):
-        LOG.debug('Exit')
+        LOG.info('Exit')
         sys.exit(0)
