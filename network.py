4b1073a6d526e47e9eed790e82c14446f1b81910
Klim Lyapin <westn23@mail.ru>
2017-05-08 16:22:44 +0000

    fix our function

--- network.py
+++ network.py
@@ -2,25 +2,27 @@ from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from log import get_logger
 
 
+LOG = get_logger()
+
 def start_tcp_service(host, port):
 
-    log = get_logger()
+    LOG = get_logger()
     tcp_socket = socket(AF_INET, SOCK_STREAM)
     tcp_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    log.debug('Set options SOL_SOCKET and SO_REUSEADDR.')
+    LOG.debug('Set options SOL_SOCKET and SO_REUSEADDR.')
     tcp_socket.bind((host, port))
-    log.debug('Bind socket to the address.')
+    LOG.debug('Bind socket to the address.')
     tcp_socket.listen(4)
-    log.debug('Make number of the maximum connections equal 4.')
+    LOG.debug('Make number of the maximum connections equal 4.')
 
     tcp_dict = {}
 
     while True:
         print('\nListening for incoming connections...')
-        log.debug('Listening for incoming connections.')
+        LOG.debug('Listening for incoming connections.')
         conn, addr = tcp_socket.accept()
-        log.debug('Accepting the connection.')
+        LOG.debug('Accepting the connection.')
         tcp_dict[addr[0]] = conn
         print(tcp_dict)
-        log.debug('Successful connection.')
+        LOG.debug('Successful connection.')
 
