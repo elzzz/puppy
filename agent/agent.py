20453ad0172e2c66cd28101aebf29090e33b8e8c
Klim Lyapin <westn23@mail.ru>
2017-07-09 21:45:15 +0000

    add reconnect agent

--- agent/agent.py
+++ agent/agent.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import time
 from socket import socket, AF_INET, SOCK_STREAM
 sys.path.insert(0, '..')
 sys.dont_write_bytecode = True
@@ -24,6 +25,30 @@ def connect_agent():
     while True:
         data = sock.recv(1024)
         if not data:
+            reconnect_agent()
             continue
         LOG.info('Received {}'.format(data))
         os.system(data)
+
+
+def reconnect_agent():
+    sock = socket(AF_INET, SOCK_STREAM)
+    toBreak = False
+    while True:
+        sock.close()
+        try:
+            connect_agent()
+            toBreak = True
+        except KeyboardInterrupt:
+            sys.exit()
+        except:
+            pass
+        if toBreak:
+            break
+        time.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        reconnect_agent()
+    except KeyboardInterrupt:
+        sys.exit()
