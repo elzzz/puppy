b70b46023971d317ca69e239f6237bd48bb9dc50
Klim Lyapin <westn23@mail.ru>
2017-08-27 13:23:16 +0400

    added working version of the table

--- server/flask_app.py
+++ server/flask_app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import json
 
 app = Flask(__name__, static_url_path="", static_folder="static")
+
 
 @app.route('/')
 def index():
@@ -9,11 +11,17 @@ def index():
 
 @app.route('/agent', methods=['GET'])
 def get_agent_list():
-    return "{'agents': {'agent_1': '1.2.3.4', 'agent_2': '9.8.7.6'}}"
+    data = {"agents": {"1.2.3.4": "socket_object_1", "8.7.6.5": "socket_object_2"}}
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found():
     return "This page does not exist", 404
 
 if __name__ == '__main__':
